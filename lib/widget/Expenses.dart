acbe4663ebb806cba00e9ba31af55f4757a9493c
Saeed Talebi <113334275+saeed561@users.noreply.github.com>
2023-08-10 00:11:27 +0330

    ExpenseTracker_OrientationLockedTOPortrateUp

--- lib/widget/Expenses.dart
+++ lib/widget/Expenses.dart
@@ -1,4 +1,5 @@
 import 'package:basic_project/widget/New_Expense.dart';
+import 'package:basic_project/widget/chart/chart.dart';
 import 'package:basic_project/widget/expenses_list/expenses_list.dart';
 import 'package:basic_project/modal/Expense.dart';
 import 'package:flutter/material.dart';
@@ -86,7 +87,7 @@ class _ExpensesState extends State<Expenses> {
       ]),
       body: Column(
         children: [
-          const Text("The Chart"),
+          Chart(expenses: _myExpenses),
           Expanded(
             child: screenContent,
           ),
