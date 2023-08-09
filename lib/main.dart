cc5675ed49cba74165984a7c73efe55e4828777c
Saeed Talebi <113334275+saeed561@users.noreply.github.com>
2023-08-09 13:10:07 +0330

    ExpenseTracker_StylingThemeOfApp_PrimaryColorAndAppbarThemeComfigurationFOrBackgroundAndFOrground

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:basic_project/widget/Expenses.dart';
 import 'package:flutter/material.dart';
 
+var KcolorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 98, 92, 188),
+);
 void main() {
-  runApp(
-    MaterialApp(
-      home: Expenses(),
-    )
-  );
+  runApp(MaterialApp(
+    theme: ThemeData().copyWith(
+      colorScheme: KcolorScheme,
+      useMaterial3: true,
+      appBarTheme: AppBarTheme().copyWith(
+        backgroundColor: KcolorScheme.onPrimaryContainer,
+        foregroundColor: KcolorScheme.onPrimary,
+      ),
+    ),
+    home: Expenses(),
+  ));
 }
