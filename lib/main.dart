cecda09a04d54107bc6cea4650a1c013c0a576ef
Saeed Talebi <113334275+saeed561@users.noreply.github.com>
2023-08-09 13:26:04 +0330

    ExpenseTracker_CardThemeSettingwasSetAndUpdated

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:basic_project/widget/Expenses.dart';
 import 'package:flutter/material.dart';
 
 var KcolorScheme = ColorScheme.fromSeed(
-  seedColor: Color.fromARGB(255, 98, 92, 188),
+  seedColor: Color.fromARGB(255, 96, 86, 180),
 );
 void main() {
   runApp(MaterialApp(
@@ -12,6 +12,10 @@ void main() {
       appBarTheme: AppBarTheme().copyWith(
         backgroundColor: KcolorScheme.onPrimaryContainer,
         foregroundColor: KcolorScheme.onPrimary,
+      ),
+      cardTheme: CardTheme().copyWith(
+        color: KcolorScheme.secondaryContainer,
+        margin: EdgeInsets.symmetric(horizontal: 14, vertical: 6),
       ),
     ),
     home: Expenses(),
